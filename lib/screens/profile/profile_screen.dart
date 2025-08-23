8280dd582c100cc5174297c762cf7f88b7e4e113
SeongJun <jun05202569@gmail.com>
2025-08-23 14:17:00 +0900

    basic frame

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('프로필'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+    );
+  }
+}
